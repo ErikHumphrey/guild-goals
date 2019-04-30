c324a89e60bac264f14ab0d5ea56d04f82996eee
Erik Humphrey <ContactErikH@gmail.com>
2019-04-30 12:47:29 -0400

    Add project files.

---
+++ Goal Wars/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Goal_Wars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string json = new WebClient().DownloadString("https://api.guildwars2.com/v2/items/24");
+            dynamic stuff = JsonConvert.DeserializeObject(json);
+            string funkyType = stuff.details.type;
+            MessageBox.Show(funkyType);
+        }
+    }
+}
